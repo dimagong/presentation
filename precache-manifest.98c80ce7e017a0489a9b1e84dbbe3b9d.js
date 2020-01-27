4173892b984500644ac81c7b9d9132cb1091d381
dimagong <dimagong@gmail.com>
2020-01-27 10:47:18 +0200

    Updates

---
+++ precache-manifest.98c80ce7e017a0489a9b1e84dbbe3b9d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85e4b9c75474b82b567089561185ad29",
+    "url": "/presentation/index.html"
+  },
+  {
+    "revision": "3855c249a1b9cc8bb4c9",
+    "url": "/presentation/static/css/main.2adbb80b.chunk.css"
+  },
+  {
+    "revision": "16209cd0629f227aa698",
+    "url": "/presentation/static/js/2.98392a7d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/presentation/static/js/2.98392a7d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3855c249a1b9cc8bb4c9",
+    "url": "/presentation/static/js/main.aa2605ca.chunk.js"
+  },
+  {
+    "revision": "e40d23729398a7c78780",
+    "url": "/presentation/static/js/runtime-main.3bddf499.js"
+  },
+  {
+    "revision": "2ef96a9e73430182386e5e2cb32175e2",
+    "url": "/presentation/static/media/Calculator.2ef96a9e.jpg"
+  },
+  {
+    "revision": "6de73d2b9993439dc25d631603618d67",
+    "url": "/presentation/static/media/Drum.6de73d2b.jpg"
+  },
+  {
+    "revision": "3f9b7b5628d5fc979fa694e1d5760653",
+    "url": "/presentation/static/media/Form.3f9b7b56.jpg"
+  },
+  {
+    "revision": "82337b6b88ccaf35470d920ad9dd104f",
+    "url": "/presentation/static/media/Markdown.82337b6b.jpg"
+  },
+  {
+    "revision": "4fa765241f911001a530d3dcb691596d",
+    "url": "/presentation/static/media/Mgid.4fa76524.jpg"
+  },
+  {
+    "revision": "42e3b03ed04471b5a3fe418e2f60a713",
+    "url": "/presentation/static/media/clock-pomodoro.42e3b03e.jpg"
+  },
+  {
+    "revision": "b54fc1c9e4f229859a1b00205f5d14c0",
+    "url": "/presentation/static/media/jsimage.b54fc1c9.png"
+  },
+  {
+    "revision": "80f536d2ae29bb0276d10e51e4cfea25",
+    "url": "/presentation/static/media/monsters.80f536d2.png"
+  },
+  {
+    "revision": "04557c1b0b790482dafb1bf4e082b69e",
+    "url": "/presentation/static/media/react.04557c1b.png"
+  },
+  {
+    "revision": "59c467536111a981d904a050d335c2be",
+    "url": "/presentation/static/media/redux.59c46753.png"
+  }
+]);
